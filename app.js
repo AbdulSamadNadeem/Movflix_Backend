24f65c460db79e3a4746572c122c8a1f1d841eac
AbdulSamadNadeem <samadnadeem372@gmail.com>
2024-12-08 23:11:58 +0500

    updated

--- app.js
+++ app.js
@@ -3,24 +3,12 @@ const app = express()
 const moviesrouter = require('./Routes/MoviesRoutes')
 const cors = require('cors')
 
-const morgan = require('morgan')
-// This is the Change
-
-
-
-
-// this is a custom MiddleWares
-const cusMw = (req , res, next)=>{
-    console.log('middle Ware is Executed')
-    req.requestedAt = new Date().toISOString();
-    next()
-}
 
 app.use(cors())
 app.use(express.static('./data/images'))
 app.use(express.json())
-app.use(morgan('dev'))
-app.use(cusMw)
+
+
 
 app.use('/api/movies' , moviesrouter)
 
